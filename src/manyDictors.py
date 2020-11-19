299c83860785171a3b163a169263c3e1ee58e603
Andrew Bykov <42899010+AndZ5556@users.noreply.github.com>
2020-11-19 03:14:49 +0300

    Task 32. Provided speech recognition
    Improved speakers identification and provided speech recognition

--- src/manyDictors.py
+++ src/manyDictors.py
@@ -1,54 +1,101 @@
 from resemblyzer import VoiceEncoder, preprocess_wav
-from pathlib import Path
+from speechRecognition import SpeechRecognition
+from exportAudio import ExportAudio
 import numpy as np
-import matplotlib.pyplot as plt
+
+import os
 
 
-class many_dictors:
+class SeveralSpeakers:
     def __init__(self):
-        self.size_cut = 10
-        self.precision = 0.005
-        self.dictors = {}
+        self.splits = []
+        self.speakers = []
+        self.timing = []
+        self.min_duration = 3
+        self.path = ''
+        self.speakers_number = 0
+        self.recognized_text = ''
 
-    def add_dictor(self, frequency, frequencies):
-        for f in self.dictors.keys():
-            if np.abs(frequency - f) < self.precision:
-                self.dictors[f] += 1
-                frequencies.append(f)
-                return
-        frequencies.append(frequency)
-        self.dictors[frequency] = 1
+    def recognize_audio(self, language, paths, export):
+        recognizer = SpeechRecognition(language)
+        res_file = open(str(self.path) + str('.txt'), "a")
+        print('Recognition started')
+        for i in range(len(paths)):
+            self.recognized_text += '\nSpeaker #' + str(self.timing[self.splits[i][0]])
+            self.recognized_text += ' %02d:%02d:' % divmod(self.splits[i][0], 60)
+            self.recognized_text += recognizer.recognize_speech(paths[i])
+            print('Recognition status: ' + str(i + 1) + '/' + str(len(paths)) + '...')
+            if not export:
+                os.remove(paths[i])
+        res_file.write(self.recognized_text)
 
-    def manyDictorsIdentification(self, wav, possible_number=False):
+    def get_splits(self):
+        tmp = 0
+        splits = []
+        for i in range(len(self.timing) - 1):
+            if self.timing[i] != self.timing[i + 1]:
+                if i - tmp <= self.min_duration:
+                    continue
+                if i + self.min_duration >= len(self.timing):
+                    continue
+                if self.timing[i + self.min_duration] != self.timing[i+1]:
+                    continue
+                if self.timing[i + self.min_duration] == self.timing[tmp]:
+                    continue
+                splits.append((tmp, i+1))
+                tmp = i + 1
+        splits.append((tmp, len(self.timing)))
+        return splits
+
+    def clear(self):
+        self.speakers_number = len(self.speakers)
+        for i in range(len(self.speakers)):
+            if len(self.speakers[i]) < self.min_duration:
+                self.speakers_number -= 1
+                for j in range(len(self.timing)):
+                    if self.timing[j] == i + 1:
+                        if j > 0:
+                            self.timing[j] = self.timing[j - 1]
+
+    def add_speaker(self, embedding, precision=0.65):
+        closet = -1
+        closet_precision = 0
+        for i in range(len(self.speakers)):
+            sum_of_precisions = 0
+            for j in range(len(self.speakers[i])):
+                sum_of_precisions += self.speakers[i][j] @ embedding
+            if len(self.speakers[i]) > 0:
+                common_precision = sum_of_precisions / len(self.speakers[i])
+                if common_precision > precision:
+                    if common_precision > closet_precision:
+                        closet_precision = common_precision
+                        closet = i
+        if closet == -1:
+            self.speakers.append([embedding])
+            self.timing.append(len(self.speakers))
+        else:
+            self.timing.append(closet + 1)
+            self.speakers[closet].append(embedding)
+
+    def several_speakers_identification(self, path, min_duration=3, return_splits=False, export=False,
+                                        recognition=False, language='en-En'):
+        self.min_duration = min_duration
+        self.path = path
+        wav = preprocess_wav(path)
         encoder = VoiceEncoder()
-        embed = encoder.embed_utterance(wav, rate=16)
+        _, embed, slices = encoder.embed_utterance(wav, return_partials=True, rate=1.16)
         np.set_printoptions(suppress=True)
-        voices = []
-        for i in embed:
-            if i > 0.05:
-                voices.append(i)
-        count = 0
-        frequency = 0
-        frequencies = []
-        for i in range(len(voices)):
-            count += 1
-            frequency += voices[i]
-            if count == self.size_cut:
-                frequency /= self.size_cut
-                self.add_dictor(frequency, frequencies)
-                count = 0
-                frequency = 0
-        count = 0
-        for i in self.dictors.keys():
-            if self.dictors[i] > 1:
-                count += 1
-        '''
-        #Visualisation
-        plt.plot(np.arange(start=0, stop=len(frequencies), step=1), frequencies, color='red')
-        plt.xlabel('frames')
-        plt.ylabel('frequency')
-        plt.show()
-        '''
-        if possible_number:
-            return count, len(self.dictors.keys())
-        return count
+        for i in range(len(embed)):
+            self.add_speaker(embed[i])
+        self.clear()
+        print('Found %d speakers' % self.speakers_number)
+        for i in range(self.speakers_number):
+            print('Speaker ' + str(i) + ': ' + str(len(self.speakers[i])) + 's')
+        self.splits = self.get_splits()
+        if recognition or export:
+            paths = ExportAudio.export(self.splits, self.path)
+            if recognition:
+                self.recognize_audio(language, paths, export)
+        if return_splits:
+            return self.speakers_number, self.splits
+        return self.speakers_number
